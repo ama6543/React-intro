e3e28c2aa5ef26d0058856be77b2b3ba825d81d9
ama6543 <amanda.j.slettli@gmail.com>
2026-01-20 16:09:44 +0100

    React intro

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <Title/>
+      <h2>Dette er kjempe gøy!</h2>
+    </main>
+  )
+}
+
+export default App
